99a7972dddf5d13244f6368435754fa30451d9d4
ahmedabd2lkarim <ahmedabd2lkarim@gmail.com>
2025-03-21 18:08:58 +0300

    test
    test

---
+++ Frontend(Angular)/Admin Dashboard/src/app/components/ChartsAndSettings/orders/orders.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { OrderService } from '../../../services/charts-and-order.service';
+
+@Component({
+  selector: 'app-orders',
+  imports: [CommonModule, MatTableModule, MatSelectModule, MatButtonModule, MatIconModule],
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.scss'
+})
+export class OrdersComponent {
+
+  orders: any[] = [];
+  displayedColumns: string[] = ['id', 'user', 'status', 'total'];
+  statusOptions = [
+    { value: 'pending', label: 'Pending' },
+    { value: 'failed', label: 'Failed' },
+    { value: 'paid', label: 'Paid' },
+    { value: 'delivered', label: 'Delivered' },
+    { value: 'canceled', label: 'Cancelled' }
+  ];
+  
+
+
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders() {
+    this.orderService.getAllOrders().subscribe({
+      next: (data) => {
+        if (!data || data.length === 0) {
+          console.warn("No orders found.");
+        }
+        console.log(data)
+        this.orders = data;
+      },
+      error: (err) => console.error('Error fetching orders:', err)
+    });
+  }
+
+  updateStatus(orderId: string, newStatus: string) {
+    this.orderService.updateOrderStatus(orderId, { status: newStatus }).subscribe(() => {
+      this.loadOrders();
+    });
+  }
+
+
+
+  getStatusClass(status: string): string {
+    switch (status) {
+      case 'pending': return 'pending';
+      case 'failed': return 'failed';
+      case 'paid': return 'paid';
+      case 'delivered': return 'delivered';
+      case 'canceled': return 'canceled';
+      default: return '';
+    }
+  }
+}
---
+++ Frontend(Angular)/Vendor Dashboard/.angular/cache/19.1.5/ProductStore/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-HLYNLL53.js";
+import "./chunk-KLQY5KQ5.js";
+import "./chunk-ZQ7WRKBG.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
